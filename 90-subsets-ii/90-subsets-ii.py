528a36ecad7fa14664ace2f9c12995e820302b7b
Nanu <nanup.personal+github@gmail.com>
2022-11-28 22:44:55 +0530

    Time: 80 ms (32.36%), Space: 14.2 MB (49.62%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
         nums.sort()
-        subsets = []
-        subsets.append([])
-        for i in range(len(nums)):
-            start = 0
-            if i > 0 and nums[i] == nums[i - 1]:
-                start = end
-            end = len(subsets)
-            for j in range(start, end):
-                subset = list(subsets[j])
+        def recurse(index, subset = []):
+            result.append(subset.copy())
+            for i in range(index, len(nums)):
+                if i != index and nums[i] == nums[i - 1]:
+                    continue
+                    
                 subset.append(nums[i])
-                subsets.append(subset)
-        return subsets
+                recurse(i + 1, subset)
+                subset.pop()
+        recurse(0)
+        return result
