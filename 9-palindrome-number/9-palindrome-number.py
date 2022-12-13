051ba39f8594fd422c21299f941735ab351ebfb3
Nanu <nanup.personal+github@gmail.com>
2022-12-13 19:09:49 +0530

    Time: 88 ms (76.33%), Space: 13.9 MB (16.50%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,7 +1,8 @@
 class Solution:
+    #time: O(N)
+    #space: O(N)
     def isPalindrome(self, x: int) -> bool:
         copy = x
-        x = abs(x)
         y = 0
         while x > 0:
             lastDigit = x % 10
