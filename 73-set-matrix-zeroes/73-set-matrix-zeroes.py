964d8d584a2b579ba4f891651b138bde4c2a2e01
Nanu Panchamurthy <nanup.personal+github@gmail.com>
2023-02-08 11:48:08 +0530

    Time: 133 ms (70.32%), Space: 14.8 MB (27.38%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -5,21 +5,20 @@ class Solution:
         """
         rows = len(matrix)
         cols = len(matrix[0])
+        
         firstColZero = False
         
-        #first iteration
         for i in range(rows):
-            if matrix[i][0] == 0:
-                firstColZero = True
-            for j in range(1, cols):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-        
-        #second iteration
+          if matrix[i][0] == 0:
+            firstColZero = True
+          for j in range(1, cols):
+            if matrix[i][j] == 0:
+              matrix[0][j] = 0
+              matrix[i][0] = 0
+              
         for i in range(rows - 1, -1, -1):
-            for j in range(cols - 1, 0, -1):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-            if firstColZero:
-                matrix[i][0] = 0
+          for j in range(cols - 1, 0, -1):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+              matrix[i][j] = 0
+          if firstColZero:
+            matrix[i][0] = 0
