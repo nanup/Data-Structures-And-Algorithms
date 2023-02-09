1f4f2609929960d4138a92d14c8210f5e108ec49
Nanu Panchamurthy <nanup.personal+github@gmail.com>
2023-02-09 16:28:12 +0530

    Time: 767 ms (64.98%), Space: 28.7 MB (10.06%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
-        currSum = nums[0]
+        maxSum = -math.inf
+        currSum = 0
         
-        def recurse(i, currSum, maxSum):
-            if i == len(nums):
-                return maxSum
+        for i in range(len(nums)):
+          currSum += nums[i]
+          
+          maxSum = max(maxSum, currSum)
+          
+          if currSum < 0:
+            currSum = 0
             
-            if nums[i] > nums[i] + currSum:
-                currSum = nums[i]
-                maxSum = max(maxSum, nums[i])
-            else:
-                currSum += nums[i]
-                maxSum = max(maxSum, currSum)
-                
-            return recurse(i + 1, currSum, maxSum)
-            
-        maxSum = recurse(1, currSum, maxSum)
         return maxSum
