291945aee775133d6542cd4c655f76440e876e45
Nanu Panchamurthy <nanup.personal+github@gmail.com>
2023-02-09 16:44:45 +0530

    Time: 37 ms (60.30%), Space: 13.9 MB (54.94%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left, mid, right = 0, 0, len(nums) - 1
-        while mid <= right:
-            if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
-                mid += 1
-                left += 1
-            elif nums[mid] == 1:
-                mid += 1
-            else:
-                nums[mid], nums[right] = nums[right], nums[mid]
-                right -= 1
+        zero, one, two = 0, 0, len(nums) - 1
+        while one <= two:
+          if nums[one] == 0:
+            nums[zero], nums[one] = nums[one], nums[zero]
+            zero += 1
+            one = zero
+          elif nums[one] == 2:
+            nums[two], nums[one] = nums[one], nums[two]
+            two -= 1
+          else:
+            one += 1
